48ba19e8a50c51a1ce7e410f802ae25331200ac0
sohee28 <amybae28@gmail.com>
2023-02-09 22:13:58 -0900

    added folders to use

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 import { Inter } from "@next/font/google";
-import styles from "./page.module.css";
+// import styles from "./page.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return <main>Hello from main!</main>;
 }
