fb0d5ad31d42f6faa5f7e15d69e3587f4951f352
sohee28 <amybae28@gmail.com>
2023-02-11 19:12:34 -0900

    added icon for navbar

--- public/icons/index.tsx
+++ public/icons/index.tsx
@@ -2,3 +2,5 @@ export { default as Play } from "./play.svg";
 export { default as Linkedin } from "./linkedin.svg";
 export { default as Email } from "./email.svg";
 export { default as Github } from "./github.svg";
+export { default as Movie } from "./movie.svg";
+export { default as WatchList } from "./watchlist.svg";
