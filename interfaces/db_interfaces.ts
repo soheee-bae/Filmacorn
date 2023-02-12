b6962b874963ec4dc67d1aa16823ad5e90bce18b
sohee28 <amybae28@gmail.com>
2023-02-11 17:06:18 -0900

    added serversideProps and interfaces for ts

---
+++ interfaces/db_interfaces.ts
@@ -0,0 +1,74 @@
+export interface FullData {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}
+
+export interface Movie {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface MovieDetail {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection: {};
+  budget: number;
+  genres: Genre[];
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: ProductionCompanies[];
+  production_countries: ProductionCountries[];
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: SpokenLanguages[];
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface Genre {
+  id: number;
+  name: string;
+}
+
+export interface ProductionCompanies {
+  id: number;
+  logo_path: string;
+  name: string;
+  origin_country: string;
+}
+
+export interface ProductionCountries {
+  iso_3166_1: string;
+  name: string;
+}
+
+export interface SpokenLanguages {
+  english_name: string;
+  iso_639_1: string;
+  name: string;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,108 @@
-export default function Home() {
+import { FullData, MovieDetail } from "@/interfaces/db_interfaces";
+import { GetServerSideProps } from "next";
+import { API_KEY, TMDB_EXTRA, TMDB_REQUEST_URL } from "../config";
+
+interface APIProps {
+  SpotLights: FullData;
+  PopularMovies: FullData;
+  UpcomingMovies: FullData;
+  TopRatedMovies: FullData;
+  ThrillerMovies: FullData;
+  WesternMovies: FullData;
+  AnimationMovies: FullData;
+  MainMovieInfo: MovieDetail;
+}
+
+export default function Home(props: APIProps) {
+  const {
+    SpotLights,
+    PopularMovies,
+    UpcomingMovies,
+    TopRatedMovies,
+    ThrillerMovies,
+    WesternMovies,
+    AnimationMovies,
+    MainMovieInfo,
+  } = props;
   return <main>Hello from main!</main>;
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  /* SpotLight */
+  const SpotLightsData = await fetch(
+    `${TMDB_REQUEST_URL}/trending/all/day${API_KEY}&include_adult=false&with_original_language=en`
+  );
+  const SpotLights = await SpotLightsData.json();
+
+  /* Popular */
+  const PopularMoviesData = await fetch(
+    `${TMDB_REQUEST_URL}/movie/popular${API_KEY}${TMDB_EXTRA}&include_adult=false&with_original_language=en`
+  );
+  const PopularMovies = await PopularMoviesData.json();
+
+  /* Upcoming */
+  const UpcomingMoviesData = await fetch(
+    `${TMDB_REQUEST_URL}/movie/upcoming${API_KEY}${TMDB_EXTRA}&include_adult=false&with_original_language=en`
+  );
+  const UpcomingMovies = await UpcomingMoviesData.json();
+
+  /* TopRated */
+  const TopRatedMoviesData = await fetch(
+    `${TMDB_REQUEST_URL}/movie/top_rated${API_KEY}${TMDB_EXTRA}&include_adult=false&with_original_language=en`
+  );
+  const TopRatedMovies = await TopRatedMoviesData.json();
+
+  /* Thriller */
+  const ThrillerMoviesData = await fetch(
+    `${TMDB_REQUEST_URL}/discover/movie${API_KEY}&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=53&with_watch_monetization_types=flatrate&with_original_language=en`
+  );
+  const ThrillerMovies = await ThrillerMoviesData.json();
+
+  /* Family */
+  const FamilyMoviesData = await fetch(
+    `${TMDB_REQUEST_URL}/discover/movie${API_KEY}&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=10751&with_watch_monetization_types=flatrate&with_original_language=en`
+  );
+  const FamilyMovies = await FamilyMoviesData.json();
+
+  /* Western */
+  const WesternMoviesData = await fetch(
+    `${TMDB_REQUEST_URL}/discover/movie${API_KEY}&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=37&with_watch_monetization_types=flatrate&with_original_language=en`
+  );
+  const WesternMovies = await WesternMoviesData.json();
+
+  /* Animation */
+  const AnimationMoviesData = await fetch(
+    `${TMDB_REQUEST_URL}/discover/movie${API_KEY}&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=16&with_watch_monetization_types=flatrate&with_original_language=en`
+  );
+  const AnimationMovies = await AnimationMoviesData.json();
+
+  /* Main Movie */
+  const MainMovie = TopRatedMovies.results[0];
+  const MainMovieData = await fetch(
+    `${TMDB_REQUEST_URL}/movie/${MainMovie.id}${API_KEY}&language=en-US`
+  );
+  const MainMovieInfo = await MainMovieData.json();
+
+  // /* Genre */
+  // const GenreData = await fetch(`${TMDB_REQUEST_URL}/genre/movie/list${API_KEY}&include_adult=false`);
+  // const Genres = await GenreData.json();
+  // const Genre = Genres.genres;
+  // const MainMovieVideoData = await fetch(
+  //   `${TMDB_REQUEST_URL}/movie/${MainMovie.id}/videos${API_KEY}`
+  // );
+  // const MainMovieVideo = await MainMovieVideoData.json();
+  // const MainMovieVideos = MainMovieVideo.results;
+
+  return {
+    props: {
+      SpotLights,
+      PopularMovies,
+      UpcomingMovies,
+      TopRatedMovies,
+      ThrillerMovies,
+      WesternMovies,
+      AnimationMovies,
+      MainMovieInfo,
+    },
+  };
+};
