fffbdc7a940868650c9b143f687d53a14ce94583
sohee28 <amybae28@gmail.com>
2023-02-14 22:54:47 -0900

    clean up

---
+++ components/DetailHeader/DetailHeader.tsx
@@ -0,0 +1,64 @@
+import { Play } from "@/icons/index";
+import { Cast, MovieDetail } from "@/interfaces/movie";
+import { Video } from "@/interfaces/video";
+import Button from "@/components/Button/Button";
+import CastList from "@/components/CastList/CastList";
+import DateTime from "@/components/DateTime/DateTime";
+import GenreList from "@/components/GenreList/GenreList";
+import styles from "./DetailHeader.module.scss";
+
+interface DetailHeaderProps {
+  movieDetail: MovieDetail;
+  cast: Cast[];
+  director: Cast[];
+  video: Video[];
+}
+
+export default function DetailHeader(props: DetailHeaderProps) {
+  const { movieDetail, cast, director, video } = props;
+
+  const genreList = movieDetail.genres;
+  const detailVideo = video?.find((v: any) => v.site === "YouTube");
+
+  const handleWatchNow = () => {
+    window.open(`https://www.youtube.com/watch?v=${detailVideo?.key}`);
+  };
+  const handleAddWatchList = () => {};
+
+  return (
+    <div className={styles.detailHeaderContent}>
+      <div className={styles.detailText}>
+        <p className={styles.detailTitle}>{movieDetail.title}</p>
+        <DateTime
+          releaseDate={movieDetail.release_date}
+          runtime={movieDetail.runtime}
+        />
+        <div className={styles.detailButtonsContainer}>
+          <div className={styles.detailButtons}>
+            <Button
+              size="lg"
+              variant="contained"
+              startIcon={<Play />}
+              onClick={handleWatchNow}
+              className={styles.detailButton}>
+              Watch Now
+            </Button>
+            <Button
+              size="lg"
+              variant="outlined"
+              onClick={handleAddWatchList}
+              className={styles.detailButton}>
+              + Add to Watchlist
+            </Button>
+          </div>
+        </div>
+        <GenreList genreList={genreList} />
+        <div className={styles.detailCasts}>
+          <CastList list={director} title="Directed by" />
+          <CastList list={cast} title="Starring" />
+        </div>
+        <p className={styles.detailOverview}>{movieDetail.overview}</p>
+      </div>
+    </div>
+  );
+}
--- components/DetailMain/DetailMain.tsx
+++ components/DetailMain/DetailMain.tsx
@@ -1,11 +1,8 @@
-import { Play } from "@/icons/index";
 import { Cast, MovieDetail } from "@/interfaces/movie";
 import { Video } from "@/interfaces/video";
-import Button from "@/components/Button/Button";
-import CastList from "@/components/CastList/CastList";
-import DateTime from "@/components/DateTime/DateTime";
-import GenreList from "@/components/GenreList/GenreList";
 import MainBackground from "@/components/MainBackground/MainBackground";
+import DetailRecom from "@/components/DetailRecom/DetailRecom";
+import DetailHeader from "@/components/DetailHeader/DetailHeader";
 import styles from "./DetailMain.module.scss";
 
 interface DetailMainProps {
@@ -18,52 +15,18 @@ interface DetailMainProps {
 export default function DetailMain(props: DetailMainProps) {
   const { movieDetail, cast, director, video } = props;
 
-  const genreList = movieDetail.genres;
   const imgSrc = movieDetail.backdrop_path || movieDetail.poster_path;
-  const detailVideo = video?.find((v: any) => v.site === "YouTube");
-
-  const handleWatchNow = () => {
-    window.open(`https://www.youtube.com/watch?v=${detailVideo?.key}`);
-  };
-  const handleAddWatchList = () => {};
 
   return (
     <div className={styles.detailMainContainer}>
       <MainBackground imgSrc={imgSrc} />
-      <div className={styles.detailMainContent}>
-        <div className={styles.detailText}>
-          <p className={styles.detailTitle}>{movieDetail.title}</p>
-          <DateTime
-            releaseDate={movieDetail.release_date}
-            runtime={movieDetail.runtime}
-          />
-          <div className={styles.detailButtonsContainer}>
-            <div className={styles.detailButtons}>
-              <Button
-                size="lg"
-                variant="contained"
-                startIcon={<Play />}
-                onClick={handleWatchNow}
-                className={styles.detailButton}>
-                Watch Now
-              </Button>
-              <Button
-                size="lg"
-                variant="outlined"
-                onClick={handleAddWatchList}
-                className={styles.detailButton}>
-                + Add to Watchlist
-              </Button>
-            </div>
-          </div>
-          <GenreList genreList={genreList} />
-          <div className={styles.detailCasts}>
-            <CastList list={director} title="Directed by" />
-            <CastList list={cast} title="Starring" />
-          </div>
-          <p className={styles.detailOverview}>{movieDetail.overview}</p>
-        </div>
-      </div>
+      <DetailHeader
+        movieDetail={movieDetail}
+        cast={cast}
+        director={director}
+        video={video}
+      />
+      <DetailRecom />
     </div>
   );
 }
