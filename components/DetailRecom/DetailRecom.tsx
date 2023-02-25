d4604897bcb8de4a7fd497820ba7a69f057c3817
sohee28 <amybae28@gmail.com>
2023-02-24 22:34:16 -0900

    fixes on the detail page recommendation layout

--- components/DetailRecom/DetailRecom.tsx
+++ components/DetailRecom/DetailRecom.tsx
@@ -14,16 +14,18 @@ export default function DetailRecom(props: DetailRecomProps) {
 
   return (
     <div className={styles.detailRecomContainer}>
-      <p className={styles.detailRecomTitle}>You Might Also Like</p>
-      <hr />
-      <div className={styles.detailRecomContent}>
-        {slicedRecomm?.map((data: Movie) => {
-          return (
-            <Link href={`/details/${data.id}`} key={data.id}>
-              <CarouselCard key={data.id} info={data} />
-            </Link>
-          );
-        })}
+      <div className={styles.detailRecomContentContainer}>
+        <p className={styles.detailRecomTitle}>You Might Also Like</p>
+        <hr />
+        <div className={styles.detailRecomContent}>
+          {slicedRecomm?.map((data: Movie) => {
+            return (
+              <Link href={`/details/${data.id}`} key={data.id}>
+                <CarouselCard key={data.id} info={data} />
+              </Link>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
