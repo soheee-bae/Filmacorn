7dfada3ac14e6cb3ea187d281ee1cacd790ec6d0
sohee28 <amybae28@gmail.com>
2023-03-01 20:17:10 -0900

    handle category side bar select highlight

--- components/CategorySidebar/CategorySidebar.tsx
+++ components/CategorySidebar/CategorySidebar.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { Genre } from "@/interfaces/basic";
 import Button from "@/components/Button/Button";
 import styles from "./CategorySidebar.module.scss";
+import { useRouter } from "next/router";
 
 interface CategorySidebarProps {
   genre: Genre[];
@@ -10,17 +11,25 @@ interface CategorySidebarProps {
 
 export default function CategorySidebar(props: CategorySidebarProps) {
   const { genre } = props;
+  const { query } = useRouter();
 
   return (
     <div className={styles.categorySidebar}>
       <Link href={`/movies`}>
-        <Button variant="text-outlined">All Movies</Button>
+        <Button variant="text-outlined" selected={!query.categoryId}>
+          All Movies
+        </Button>
       </Link>
-      {genre.map((genre) => (
-        <Link href={`/movies/${genre.id}`} key={genre.id}>
-          <Button variant="text-outlined">{genre.name}</Button>
-        </Link>
-      ))}
+      {genre.map((genre) => {
+        const selected = Number(query.categoryId) === genre?.id;
+        return (
+          <Link href={`/movies/${genre.id}`} key={genre.id}>
+            <Button variant="text-outlined" selected={selected}>
+              {genre.name}
+            </Button>
+          </Link>
+        );
+      })}
     </div>
   );
 }
